5ef0c23d0f26ca9cc3b7acabaa844c45b3ae3ea8
mclenard <empyrean.ablaze@gmail.com>
2018-04-16 17:28:28 -0400

    finished HW 14

--- model.py
+++ model.py
@@ -4,11 +4,14 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+try:
+    idnum = int(entries[0]['id']) + 1
+except:
+    idnum = 0
 
 def init(app):
     global entries
     try:
-
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
         f.close()
@@ -21,10 +24,12 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, idnum
     now = datetime.now()
-    time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    #time_string = now.strftime("%b %d, %Y %-I:%M %p")
+    time_string = str(now)
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": idnum}
+    idnum += 1
     entries.insert(0, entry) ## add to front of list
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
@@ -33,3 +38,20 @@ def add_entry(name, text):
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
+
+def delete_entry(id):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    for ent in entries:
+        try:
+            if ent["id"] == int(id):
+                entries.remove(ent)
+                print("Entry removed.")
+        except:
+            print("This entry does not have an ID number.")
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not open guestbook file to delete entry.")
